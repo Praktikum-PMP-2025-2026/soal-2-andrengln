9ad1e3126639b4dd7533f718f96cb74f7e5bf6e8
andrengln <andreasnainggolan32@gmail.com>
2026-04-15 10:53:01 +0700

    Add files via upload

---
+++ Soal 2.c
@@ -0,0 +1,41 @@
+// Modul 01 - Overview of C Language
+// Nama = Andreas Nainggolan
+// Hari/Tanggal = Rabu/15 April 2026
+
+
+#include <stdio.h>
+
+int main(){
+    int x;
+    int nilai_masukan[x];
+    printf("Jumlah nilai ");
+    scanf("%d", &x);
+
+
+    for(int i = 0; i < x; i++){
+        printf("Nilai ke-%i\n", i);
+    }
+
+
+    while (int i = 0; i < 100; i++){
+        int hasil = 0
+
+        if(nilai_Masukan[i]%2 = 0){
+            hasil = int nilai_Masukan[i]/2
+            while (nilai_Masukan[i] != 1)
+            hasil += 1
+        
+
+        } else { (nilai_Masukan[i]%2 != 0)
+            hasil = (3*int nilai_Masukan[i])+1
+            while (nilai_Masukan[i] != 1)
+            hasil += 1
+        }
+    
+    return 0;
+    
+
+    printf("LANGKAH : %d\n", hasil);
+    
+
+}
